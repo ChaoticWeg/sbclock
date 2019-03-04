4204d7509731c1b6574aeb9907f5a08b99559d4c
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-04 01:26:20 -0600

    add ability to kill tagline updater and instead display a message
    cancel this and return to normal operation by calling init() again

--- scripts/tagline.js
+++ scripts/tagline.js
@@ -2,6 +2,7 @@ const Tagline = (() => {
 
     let exposed = {};
     let CHANGE_INTERVAL_MS = 45000;
+    let next_exec = null;
 
     let update = () => {
         let nextTagline = TaglineUtils.random();
@@ -9,13 +10,36 @@ const Tagline = (() => {
         $('.tagline').fadeOut('slow', () => {
             $('.tagline').text(nextTagline);
             $('.tagline').fadeIn('slow', () => {
-                setTimeout(update, CHANGE_INTERVAL_MS);
-            })
+                next_exec = setTimeout(update, CHANGE_INTERVAL_MS);
+            });
         });
     };
 
     exposed.init = () => {
         update();
+    };
+
+    exposed.kill = (message = null) => {
+        return new Promise((resolve, _) => {
+
+            // cancel next update if queued
+            if (next_exec !== null) {
+                clearTimeout(next_exec);
+                next_exec = null;
+            }
+
+            // fade out
+            $('.tagline').fadeOut('slow', () => {
+                // if no message was provided, we're done
+                if (message === null) {
+                    return resolve();
+                }
+
+                // message was provided; set text and fade back in
+                $('.tagline').text(message);
+                $('.tagline').fadeIn('slow', resolve);
+            });
+        });
     };
 
     return exposed;
