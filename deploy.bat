de8aa7c36a64706c02e754aab65aff618485953f
ChaoticWeg <shawn@chaoticweg.cc>
2019-03-03 19:53:35 -0600

    why not; take a gander at my deploy script

---
+++ deploy.bat
@@ -0,0 +1,16 @@
+@echo off
+
+@REM xcopy to stage files for deployment
+if not exist ".\staging" mkdir staging
+echo staging files for deployment...
+robocopy . .\staging /e /purge /xo /xd staging .git .vscode /xf LICENSE .gitignore deploy.bat 1>NUL
+
+@REM scp to put files
+echo deploying files...
+pushd staging
+scp -r . mobile@10.10.1.123:/var/mobile/Library/SBHTML/sbclock 1>NUL
+popd
+
+@REM respring
+echo politely requesting respring...
+plink -ssh mobile@10.10.1.123 -i %userprofile%\.ssh\id_rsa.ppk "killall -9 SpringBoard"
